61d5f3e4c7cea3474119cda15f9f25312b7845e0
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-05-27 22:08:02 +0200

    handleSubmit is now async/await

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,22 +1,25 @@
 import React, {useState} from "react"
-import {createUserWithEmailAndPassword} from "firebase/auth";
+import {createUserWithEmailAndPassword, User} from "firebase/auth";
 import {auth} from "../services/firebase.ts";
+
 
 
 const SignUp = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 
-	const handleSubmit = (e: React.FormEvent) => {
+	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
-		createUserWithEmailAndPassword(auth, email, password)
-			.then((userCredential) => {
-				const user = userCredential.user
-				console.log(user)
-			})
-			.catch((err) => {
-				console.log(err.code)
-			})
+		try {
+			const cred = await createUserWithEmailAndPassword(auth, email, password)
+			console.log(cred)
+			console.log(cred.user.uid)
+
+		} catch (err) {
+			console.log(err)
+		}
+
+
 	}
 
 	return (
